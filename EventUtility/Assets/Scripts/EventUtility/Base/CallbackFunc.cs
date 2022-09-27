dddb8a59359be8ef8ea98ecfffd7a4d45e3a6e03
Cookdaso <yuanhaoo5234@gmail.com>
2022-09-27 13:08:22 +0800

    repository initialize

--- EventUtility/Assets/Scripts/EventUtility/Base/CallbackFunc.cs
+++
@@ -1,8 +0,0 @@
-//you can set more callback functions via overload
-//note that the generic of callback func must same as the generic of listener
-public delegate void CallbackFunc();
-public delegate void CallbackFunc<T>(T param);
-public delegate void CallbackFunc<T,X>(T param1, X param2);
-public delegate void CallbackFunc<X, Y, Z>(X param1, Y param2, Z param3);
-public delegate void CallbackFunc<X, Y, Z, W>(X param1, Y param2, Z param3, W param4);
-public delegate void CallbackFunc<X, Y, Z, W, T>(X param1, Y param2, Z param3, W param4, T param5);
--- EventUtility/Assets/Scripts/EventUtility/Base/EventTypes.cs
+++
@@ -1,9 +0,0 @@
-﻿//you can add more event type here as enum
-//recommend to categorize event types for management
-public enum EventType
-{
-    ShowText,
-    MoveToDestination,
-    SetColor,
-    ChangeColor,
-}
